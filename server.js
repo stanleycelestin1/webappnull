c116cdd64be3ebc24829b2d902e49a5514b1f793
Stanley Celestin <stanleycelestin1@gmail.comgit config --global user.namegit config --global user.email stanleycelestin1@gmail.com>
2017-08-27 17:40:57 -0400

    Copleted Backend

--- server.js
+++ server.js
@@ -16,11 +16,33 @@ var requestHandler = function(request, response) {
     HINT: explore the request object and its properties 
     http://stackoverflow.com/questions/17251553/nodejs-request-object-documentation
    */
+
+
+  if (parsedUrl.pathname == '/listings'){
+    response.writeHead(200, {'Content-Type' : 'application/json'});
+    response.write(listingData);
+  }
+  else{
+    response.writeHead(404, {'Content-Type' : 'text/plain'});
+    response.write('Bad gateway error');
+  }
+
+  response.end();
 };
+
 
 fs.readFile('listings.json', 'utf8', function(err, data) {
   /*
     This callback function should save the data in the listingData variable, 
     then start the server. 
    */
+   if (err){
+    throw err;
+   }
+
+   listingData = data;
+
+   server = http.createServer(requestHandler).listen(port);
 });
+
+console.log('Server Started...\n');
